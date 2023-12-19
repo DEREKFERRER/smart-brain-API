af4f3398c498d323f5854f7906cd3a8b4c1db535
dk <froderick663@gmail.com>
2023-12-19 21:54:50 +0800

    updated

--- server.js
+++ server.js
@@ -29,15 +29,16 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+app.get('/', (req, res)=> {res.send(database.users)})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-                                                //this is called dependency injection we're injecting whatever dependencies
+                                                                   //this is called dependency injection we're injecting whatever dependencies
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
